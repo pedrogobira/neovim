85f69272c5df926caeb7c0d73a76c7d679466b43
Pedro Gobira Pereira <pedro.pereira_p1it@totalexpress.com.br>
2023-04-27 15:25:39 -0300

    chore: remove white lines

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,5 +8,3 @@ vim.keymap.set("n", "<leader>hh", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<leader>hj", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<leader>hk", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<leader>hl", function() ui.nav_file(4) end)
-
-
