9f5aca9a899b39a593ea90558c834461d4892275
Pedro Gobira Pereira <64466694+pedrogobira@users.noreply.github.com>
2023-07-30 12:45:21 -0300

    fix: order of keymaps

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,8 +1,8 @@
 local ls = require("luasnip")
 
 vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
 vim.keymap.set({"i", "s"}, "<C-H>", function() ls.jump(-1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
 
 vim.keymap.set({"i", "s"}, "<C-E>", function()
 	if ls.choice_active() then
