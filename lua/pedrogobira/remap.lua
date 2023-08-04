7b4a9dbdb4ad5b0fc2cfe1b7919051bead62ef9d
Pedro Gobira Pereira <64466694+pedrogobira@users.noreply.github.com>
2023-08-04 00:01:57 -0300

    feat: add keymap to resize with arrows

--- lua/pedrogobira/remap.lua
+++ lua/pedrogobira/remap.lua
@@ -3,3 +3,10 @@ vim.keymap.set("n", "<leader>oe", vim.cmd.Ex)
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>bd", ":bdelete<CR>")
+
+-- Resize with arrows
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
