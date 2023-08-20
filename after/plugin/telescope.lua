54ea04bda76504f797b3f0e63424e0dc5f22f771
Pedro Gobira Pereira <64466694+pedrogobira@users.noreply.github.com>
2023-08-20 20:57:56 -0300

    feat: add rules for telescope so that it can ignore some folders

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,13 @@
+local telescope = require('telescope')
+
+telescope.setup {
+  defaults = {
+   file_ignore_patterns = {
+     "node_modules", "build", "dist", "vendor"
+   },
+  }
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
