5b15f38681a50ad149d74473f0161706386e602d
Pedro Gobira Pereira <pedro.pereira_p1it@totalexpress.com.br>
2023-09-05 00:30:00 -0300

    refactor: remove autoinstall of treesitter support for some langs

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "lua", "go", "javascript", "typescript", "php", "rust" },
+  ensure_installed = { "lua" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
