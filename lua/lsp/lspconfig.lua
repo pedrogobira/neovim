d6667a5b6f4b5352274a6472400f004a77adcbe4
Pedro Gobira Pereira <pedro.pereira_p1it@totalexpress.com.br>
2024-01-23 21:14:59 -0300

    feat: add phpactor in lsp servers list

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -124,7 +124,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = { "pyright", "html", "cssls", "tsserver", "gopls", "templ" }
+local servers = { "pyright", "html", "cssls", "tsserver", "gopls", "templ", "phpactor" }
 
 -- Call setup
 for _, lsp in ipairs(servers) do
