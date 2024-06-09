321ac341dacbc49fefdeb0f4eb8fa966fe4f3cf7
Pedro Gobira Pereira <pedro.gobira@totalexpress.com.br>
2024-06-09 02:17:03 -0300

    feat: add gruvbox as theme

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -92,6 +92,11 @@ lazy.setup({
     {
       "RRethy/base16-nvim",
       priority = 1000, -- Ensure it loads first
-    }
+    },
+    {
+      "neanias/everforest-nvim",
+      priority = 1000, -- Ensure it loads first
+    },
+    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
 	},
 })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,20 @@
 -- To disable highlights for supported plugin(s), call the `with_config` function **before** setting the colorscheme.
 -- These are the defaults.
-require('base16-colorscheme').with_config({
-    telescope = false,
-    indentblankline = false,
-    notify = false,
-    ts_rainbow = false,
-    cmp = false,
-    illuminate = false,
-    dapui = false,
-})
+-- require('base16-colorscheme').with_config({
+--     telescope = false,
+--     indentblankline = false,
+--     notify = false,
+--     ts_rainbow = false,
+--     cmp = false,
+--     illuminate = false,
+--     dapui = false,
+-- })
 -- You can get the base16 colors **after** setting the colorscheme by name (base01, base02, etc.)
 -- local color = require('base16-colorscheme').colors.base01
-vim.cmd("colorscheme base16-primer-dark")
+-- vim.cmd("colorscheme base16-primer-dark")
+-- require("everforest").setup({
+--   background = "hard"
+-- })
+-- vim.cmd("colorscheme everforest")
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
