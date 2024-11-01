74791c1fefd64bc54626a8b1a523f57e74ef4e31
Pedro Gobira Pereira <pedro.gobira@totalexpress.com.br>
2024-11-01 02:04:13 -0300

    feat: python-lsp-server

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -124,7 +124,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = { "pyright", "html", "cssls", "tsserver", "gopls", "intelephense", "rust_analyzer" }
+local servers = { "pylsp", "html", "cssls", "ts_ls", "gopls", "intelephense", "rust_analyzer" }
 
 -- Call setup
 for _, lsp in ipairs(servers) do
